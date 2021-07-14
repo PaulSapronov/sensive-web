5aff00677ccab798e82ff9b5a144b52e2436a2ee
Paul <pavel.sapronov@mail.ru>
2021-07-14 12:44:40 +0700

    Created content-post page

--- single.php
+++ single.php
@@ -22,48 +22,29 @@
 <section class="blog-post-area section-margin">
   <div class="container">
     <div class="row">
-      <div class="col-lg-8">
-        <div class="main_blog_details">
-          <img class="img-fluid" src="img/blog/blog4.png" alt="">
-          <a href="#">
-            <h4>Cartridge Is Better Than Ever <br /> A Discount Toner</h4>
-          </a>
-          <div class="user_details">
-            <div class="float-left">
-              <a href="#">Lifestyle</a>
-              <a href="#">Gadget</a>
-            </div>
-            <div class="float-right mt-sm-0 mt-3">
-              <div class="media">
-                <div class="media-body">
-                  <h5>Mark wiens</h5>
-                  <p>12 Dec, 2017 11:21 am</p>
-                </div>
-                <div class="d-flex">
-                  <img width="42" height="42" src="img/blog/user-img.png" alt="">
-                </div>
-              </div>
-            </div>
-          </div>
-          <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower</p>
-          <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.</p>
-          <blockquote class="blockquote">
-            <p class="mb-0">MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training.</p>
-          </blockquote>
-          <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower</p>
-          <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower</p>
-          <div class="news_d_footer flex-column flex-sm-row">
-            <a href="#"><span class="align-middle mr-2"><i class="ti-heart"></i></span>Lily and 4 people like this</a>
-            <a class="justify-content-sm-center ml-sm-auto mt-sm-0 mt-2" href="#"><span class="align-middle mr-2"><i class="ti-themify-favicon"></i></span>06 Comments</a>
-            <div class="news_socail ml-sm-auto mt-sm-0 mt-2">
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-dribbble"></i></a>
-              <a href="#"><i class="fab fa-behance"></i></a>
-            </div>
-          </div>
-        </div>
 
+      <main class="col-lg-8">
+
+        <?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sensive' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sensive' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
 
         <div class="navigation-area">
           <div class="row">
@@ -97,6 +78,8 @@
             </div>
           </div>
         </div>
+
+
         <div class="comments-area">
           <h4>05 Comments</h4>
           <div class="comment-list">
@@ -215,151 +198,12 @@
             <a href="#" class="button submit_btn">Post Comment</a>
           </form>
         </div>
-      </div>
+      </main>
 
       <!-- Start Blog Post Siddebar -->
-      <div class="col-lg-4 sidebar-widgets">
-        <div class="widget-wrap">
-          <div class="single-sidebar-widget newsletter-widget">
-            <form action="#">
-              <div class="d-flex flex-row">
-                <input class="form-control" name="q" placeholder="Search" required="" type="text" value="">
-                <button class="click-btn btn btn-default bbtns"><i class="ti-search"></i></button>
-              </div>
-            </form>
-          </div>
-
-          <!-- <div class="single-sidebar-widget newsletter-widget">
-                <h4 class="single-sidebar-widget__title">Newsletter</h4>
-                <div class="form-group mt-30">
-                  <div class="col-autos">
-                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email" onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Enter email'">
-                  </div>
-                </div>
-                <button class="bbtns d-block mt-20 w-100">Subcribe</button>
-              </div> -->
-
-          <div class="single-sidebar-widget post-category-widget">
-            <h4 class="single-sidebar-widget__title">Category</h4>
-            <ul class="cat-list mt-20">
-              <li>
-                <a href="archive.html" class="d-flex justify-content-between">
-                  <p>Technology</p>
-                  <p>(03)</p>
-                </a>
-              </li>
-              <li>
-                <a href="archive.html" class="d-flex justify-content-between">
-                  <p>Software</p>
-                  <p>(09)</p>
-                </a>
-              </li>
-              <li>
-                <a href="archive.html" class="d-flex justify-content-between">
-                  <p>Lifestyle</p>
-                  <p>(12)</p>
-                </a>
-              </li>
-              <li>
-                <a href="archive.html" class="d-flex justify-content-between">
-                  <p>Shopping</p>
-                  <p>(02)</p>
-                </a>
-              </li>
-              <li>
-                <a href="archive.html" class="d-flex justify-content-between">
-                  <p>Food</p>
-                  <p>(10)</p>
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="single-sidebar-widget popular-post-widget">
-            <h4 class="single-sidebar-widget__title">Popular Post</h4>
-            <div class="popular-post-list">
-              <div class="single-post-list">
-                <div class="thumb">
-                  <img class="card-img rounded-0" src="img/blog/thumb/thumb1.png" alt="">
-                  <ul class="thumb-info">
-                    <li><a href="#">Adam Colinge</a></li>
-                    <li><a href="#">Dec 15</a></li>
-                  </ul>
-                </div>
-                <div class="details mt-20">
-                  <a href="blog-single.html">
-                    <h6>Accused of assaulting flight attendant miktake alaways</h6>
-                  </a>
-                </div>
-              </div>
-              <div class="single-post-list">
-                <div class="thumb">
-                  <img class="card-img rounded-0" src="img/blog/thumb/thumb2.png" alt="">
-                  <ul class="thumb-info">
-                    <li><a href="#">Adam Colinge</a></li>
-                    <li><a href="#">Dec 15</a></li>
-                  </ul>
-                </div>
-                <div class="details mt-20">
-                  <a href="blog-single.html">
-                    <h6>Tennessee outback steakhouse the
-                      worker diagnosed</h6>
-                  </a>
-                </div>
-              </div>
-              <div class="single-post-list">
-                <div class="thumb">
-                  <img class="card-img rounded-0" src="img/blog/thumb/thumb3.png" alt="">
-                  <ul class="thumb-info">
-                    <li><a href="#">Adam Colinge</a></li>
-                    <li><a href="#">Dec 15</a></li>
-                  </ul>
-                </div>
-                <div class="details mt-20">
-                  <a href="blog-single.html">
-                    <h6>Tennessee outback steakhouse the
-                      worker diagnosed</h6>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="single-sidebar-widget tag_cloud_widget">
-            <h4 class="single-sidebar-widget__title">Tags</h4>
-            <ul class="list">
-              <li>
-                <a href="#">project</a>
-              </li>
-              <li>
-                <a href="#">love</a>
-              </li>
-              <li>
-                <a href="#">technology</a>
-              </li>
-              <li>
-                <a href="#">travel</a>
-              </li>
-              <li>
-                <a href="#">software</a>
-              </li>
-              <li>
-                <a href="#">life style</a>
-              </li>
-              <li>
-                <a href="#">design</a>
-              </li>
-              <li>
-                <a href="#">illustration</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <?php get_sidebar(); ?>
+      <!-- End Blog Post Siddebar -->
     </div>
-    <!-- End Blog Post Siddebar -->
-  </div>
 </section>
 <!--================ End Blog Post Area =================-->
 
---
+++ template-parts/content-post.php
@@ -0,0 +1,36 @@
+<div class="main_blog_details">
+  <img class="img-fluid" src="img/blog/blog4.png" alt="">
+  <a href="#">
+    <h4><?php the_title(); ?></h4>
+  </a>
+  <div class="user_details">
+    <div class="float-left">
+      <a href="#">Lifestyle</a>
+      <a href="#">Gadget</a>
+    </div>
+    <div class="float-right mt-sm-0 mt-3">
+      <div class="media">
+        <div class="media-body">
+          <h5>Mark wiens</h5>
+          <p>12 Dec, 2017 11:21 am</p>
+        </div>
+        <div class="d-flex">
+          <img width="42" height="42" src="img/blog/user-img.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php the_content(); ?>
+
+  <div class="news_d_footer flex-column flex-sm-row">
+    <a href="#"><span class="align-middle mr-2"><i class="ti-heart"></i></span>Lily and 4 people like this</a>
+    <a class="justify-content-sm-center ml-sm-auto mt-sm-0 mt-2" href="#"><span class="align-middle mr-2"><i class="ti-themify-favicon"></i></span>06 Comments</a>
+    <div class="news_socail ml-sm-auto mt-sm-0 mt-2">
+      <a href="#"><i class="fab fa-facebook-f"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-dribbble"></i></a>
+      <a href="#"><i class="fab fa-behance"></i></a>
+    </div>
+  </div>
+</div>

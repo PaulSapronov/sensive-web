1bdeb8699fc52d50bea5d8ccdc002a83b1befe12
Paul <pavel.sapronov@mail.ru>
2021-07-15 03:40:16 +0700

    Created Comment Walker&Comment Block

--- comments.php
+++ comments.php
@@ -1,5 +1,76 @@
+<?php
+
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments-area">
+
+  <?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) :
+		?>
+  <h4 class="comments-title">
+
+    <?php
+			$sensive_comment_count = get_comments_number();
+			if ( '1' === $sensive_comment_count ) {
+				printf(
+					/* translators: 1: title. */
+					esc_html__( 'Один комментарий &ldquo;%1$s&rdquo;', 'sensive' ),
+					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+				);
+			} else {
+				printf( 
+					/* translators: 1: comment count number, 2: title. */
+					esc_html( _nx( '%1$s комментарий &ldquo;%2$s&rdquo;', '%1$s Комментариев', $sensive_comment_count, 'comments title', 'sensive' ) ),
+					number_format_i18n( $sensive_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+				);
+			}
+			?>
+  </h4><!-- .comments-title -->
+
+  <?php the_comments_navigation(); ?>
+
+  <div class="comment-list">
+    <?php
+			wp_list_comments(
+				array(
+					'walker'            => new Bootstrap_Walker_Comment(),
+          'max_depth'         => '2',
+          'style'             => 'ol',
+          'type'              => 'all',
+          'reply_text'        => 'Ответить',
+          'per_page'          => '10',
+          'avatar_size'       => 60,
+          'format'            => 'html5', // или xhtml, если HTML5 не поддерживается темой
+          'echo'              => true,     // true или false
+				)
+			);
+			?>
+  </div><!-- .comment-list -->
+
+  <?php
+		the_comments_navigation();
+
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() ) :
+			?>
+  <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'sensive' ); ?></p>
+  <?php
+		endif;
+
+	endif; // Check for have_comments().
+
+	comment_form();
+	?>
+
+</div><!-- #comments -->
+
 <div class="comments-area">
-  <h4>05 Comments</h4>
+
   <div class="comment-list">
     <div class="single-comment justify-content-between d-flex">
       <div class="user justify-content-between d-flex">
@@ -14,9 +85,7 @@
           </p>
         </div>
       </div>
-      <div class="reply-btn">
-        <a href="" class="btn-reply text-uppercase">reply</a>
-      </div>
+      <div class="reply-btn"><a href="" class="btn-reply text-uppercase">reply</a></div>
     </div>
   </div>
   <div class="comment-list left-padding">
